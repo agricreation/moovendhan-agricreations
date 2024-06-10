3080de07b9fba2e39225f620e6e75353639d42c1
agricreations <moovendhanagriculture@gmail.com>
2024-06-10 06:07:54 +0530

    removing conflict file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import Image from "next/image";
 import {
   ModeToggle
 } from "@/components/dark-mode"
-import Homes from "@/pages/index"
 
 
 export default function Home() {
   return (
     <>
      <main>
-      {/* <Homes /> */}
       <ModeToggle />
     </main>
     </>
